dac113891ffed8e93b3acf8116a82c0b57fa48c5
167544 <167544@ust-global.com>
2024-06-18 17:55:04 +0530

    trial cisemsbackend june

--- Routes/SendOTP.js
+++ Routes/SendOTP.js
@@ -1,6 +1,8 @@
 // Import required modules
 const express = require('express');
 const nodemailer = require('nodemailer');
+const { getDB } = require('./dbconnection');
+const { verifyIfManager } = require('../controllers/user');
 
 // Create an instance of Express router
 const router = express.Router();
@@ -24,26 +26,60 @@ const transporter = nodemailer.createTransport({
 
 // Define the route for sending OTP
 router.post('/', async (req, res) => {
-    const { email } = req.body;
-    if (!email) {
-        return res.status(400).json({ message: 'Email address is required' });
+    const db = getDB()
+    let { email, employeeId, isAdmin } = req.body;
+    if (!email || !employeeId) {
+        return res.status(400).json({ message: 'Email address and employee id is required' });
+    }
+    console.log('^^^^^^^^^^^^^^^^^^',employeeId,email)
+    employeeId = !isNaN(employeeId) ? Number(employeeId) : employeeId
+    const query =  { "Employee ID": employeeId, "Employee Email": email }
+    const employee = await db.collection("employeeDetails").findOne(query);
+    console.log('1111testing otp failed ######', query,employee)
+    console.log('2222testing otp failed ######', employee);
+    if (!employee) {
+        return res.status(400).json({ message: "Employee details not found" });
     }
 
     const otp = generateOTP();
     otpDatabase[email] = otp;
 
+    let emailSubject = `Your OTP to create account with CIS UST`;
+    let emailBody = `Your OTP to create account with CIS UST is ${ otp }`;
+    let apiResponseMessage = 'OTP sent successfully'
+
+    let isManager = verifyIfManager(email);
+    console.log({ isAdmin, isManager })
+
+    if (isAdmin && !isManager) {
+        return res.status(400).json({ message: "Only managers can register as admin. Try registering as a user!" });
+    }
+
+    if (!isAdmin && isManager) {
+        return res.status(400).json({ message: "You are a Manager. Kindly create an Admin account!" });
+    }
+
+    if (isAdmin) {
+        emailSubject = `OTP for ${ employee["Employee Name"] } to create account with CIS UST`;
+        emailBody = `${ employee["Employee Name"]} has initiated an account creation with CIS UST as an Admin User. The OTP is ${ otp }.\nKindly share this OTP with them to finish their account creation.`
+        apiResponseMessage = `OTP has been shared with your manager - ${ employee["1st Manager"] }. Kindly get in touch with them to get the OTP to complete your account creation` 
+    }
+
+    let emailOfFirstManagerOfAdminEmployee = employee["1st Manager Email ID"];
+    console.log(`Sending otp ${ otp } to reporting manager of ${ employee["Employee Name"] }`, emailOfFirstManagerOfAdminEmployee)
+
     // Send OTP via email
     const mailOptions = {
         from: 'ust.cis.ems@gmail.com', // Sender email address
-        to: 'nehamary.baby@ust.com, vipin.kumarnair@ust.com', //change your email
-        subject: 'OTP Verification for '+email,
-        text: `The OTP code generated for `+email+` is: ${otp}`
+        //to: emailOfFirstManagerOfAdminEmployee,
+         to: 'nehamary.baby@ust.com, vipin.kumarnair@ust.com, edwin.joseph12@ust.com', //change your email
+        subject: emailSubject,
+        text: emailBody
     };
-
+    
     try {
-        const info = await transporter.sendMail(mailOptions);
-        console.log('OTP sent successfully:', info.response);
-        res.status(200).json({ message: 'OTP sent successfully' });
+        transporter.sendMail(mailOptions);
+        res.status(200).json({ message: apiResponseMessage });
     } catch (error) {
         console.error('Error sending OTP:', error);
         res.status(500).json({ message: 'Error sending OTP' });
